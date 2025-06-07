0dc1ef66a43503ee3ec62ae31c5d51c23795118d
Emmelie83 <ej224sk@student.lnu.se>
2025-06-07 21:20:44 +0200

    Change rules text

--- src/components/rules/Rules.tsx
+++ src/components/rules/Rules.tsx
@@ -2,35 +2,42 @@ import React from "react";
 import Paper from "../../assets/img/Paper.png";
 
 const Rules: React.FC = () => (
-	<section id="rules"
+	<section
+		id="rules"
 		className="flex justify-center items-center bg-no-repeat bg-center -mt-60 bg-contain h-[750px] mb-16"
 		style={{ backgroundImage: `url(${Paper})` }}>
 		<div className="text-center w-full max-w-2xl px-6 -mt-6">
 			<h2 className="mb-3 text-shadow-sm">
-				Tala rövarspråket som en äkta sjörövare!
+				Tala rövarspråket som en riktig sjörövare!
 			</h2>
 			<p>
-				Rövarspråket är ett lekfullt kodspråk som blev känt genom Astrid
-				Lindgrens böcker om mästerdetektiven Kalle Blomkvist. Det bygger
-				på en enkel regel:{" "}
-				<strong>
-					Efter varje konsonant lägger man till ett "o" och sedan
-					samma konsonant igen:
-				</strong>
+				Rövarspråket är ett kul kodspråk som blev känt i Astrid
+				Lindgrens böcker om Kalle Blomkvist. Reglerna är enkla:
 			</p>
-			<p className="my-4">rövarspråket → rorövovarorsospoproråkoketot</p>
-			<h4 className="font-semibold my-3">Men det finns undantag!</h4>
-			<p className="mb-3">
-				Eftersom rövarspråket i grunden är tänkt att talas högt, gör man
-				ibland avsteg från huvudregeln för att underlätta uttalet:
+			<ul>
+				<li className="mt-4">
+					<strong>
+						Efter varje konsonant lägger du till ett "o" och samma
+						konsonant igen.
+					</strong>
+				<p>Exempel: rövarspråket → rorövovarorsospoproråkoketot</p></li>
+			</ul>
+			<p className="font-semibold my-3">
+				Men för att göra språket lättare att säga högt finns vissa
+				undantag:
 			</p>
-
-			<ul className="leading-8">
-				<li>”x” blir till ”ks”: kex → kokekoksos</li>
-				<li>”ck” blir till ”kk”: tack → totakokkok</li>
+			<ul>
 				<li>
-					sje- och tje-ljud betraktas som en enda konsonant: choklad →
-					chochakokloladod
+					<strong>”x” uttalas som ”ks”</strong>{" "}
+					<p>Exempel: kex → kokekoksos</p>
+				</li>
+				<li>
+					<strong>”ck” uttalas som ”kk”</strong>{" "}
+					<p>Exempel: tack → totakokkok</p>
+				</li>
+				<li>
+					<strong>sje- och tje-ljud räknas som en konsonant</strong>{" "}
+					<p>Exempel: choklad → chochakokloladod</p>
 				</li>
 			</ul>
 		</div>
