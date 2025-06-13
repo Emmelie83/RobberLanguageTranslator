9a28c6e2aff4faa3495232f062bd86493f3fd04c
Emmelie83 <ej224sk@student.lnu.se>
2025-06-13 14:29:09 +0200

    Change path to hero image

--- src/components/hero/HeroSection.tsx
+++ src/components/hero/HeroSection.tsx
@@ -4,7 +4,7 @@ import Navbar from "../Navbar";
 const Hero: React.FC = () => (
 	<div id="home" className="">
 		<Navbar />
-		<div className="bg-cover bg-left h-[640px] flex flex-col justify-center items-center bg-[url('../../public/hero.png')]"></div>
+		<div className="bg-cover bg-left h-[640px] flex flex-col justify-center items-center bg-[url('./hero.png')]"></div>
 	</div>
 );
 
