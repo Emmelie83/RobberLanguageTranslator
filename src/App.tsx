055428ab0f30a31e50d1295ff5c837bcd2d68491
Emmelie83 <ej224sk@student.lnu.se>
2025-06-13 14:04:55 +0200

    Add imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Contact } from "lucide-react";
 import "./App.css";
-import Contact from "./components/contact/ContactSection";
 import Footer from "./components/Footer";
 import Hero from "./components/hero/HeroSection";
 import Test from "./components/language-test/LanguageTest";
 import Rules from "./components/rules/Rules";
 import Translator from "./components/translator/TranslatorSection";
+
 import "./index.css";
 
 
--- src/components/hero/HeroSection.tsx
+++ src/components/hero/HeroSection.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../Navbar";
 
 
+
 const Hero: React.FC = () => (
 	<div id="home" className="">
 		<Navbar />
