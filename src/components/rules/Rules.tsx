cd751ffa0c8defb5f04d37399c267880bd9f6471
Emmelie83 <ej224sk@student.lnu.se>
2025-06-16 23:16:31 +0200

    Change div for rules background to make it suitable for smaller screens

--- src/components/rules/Rules.tsx
+++ src/components/rules/Rules.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const Rules: React.FC = () => (
 	<section
 		id="rules"
-		className="rules-content flex justify-center bg-no-repeat bg-center -mt-70 bg-contain h-[850px] pb-60">
-		<div className="text-center w-full max-w-2xl px-6 mt-20">
+		className="">
+		<div className="rules-content flex flex-col justify-center bg-no-repeat bg-center bg-contain h-[850px] -mt-40 lg:-mt-70 text-center w-full max-w-2xl px-3">
 			<h2 className="mb-3 text-shadow-sm">
 				Tala rövarspråket som en riktig sjörövare!
 			</h2>
